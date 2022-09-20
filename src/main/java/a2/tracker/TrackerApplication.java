6ee5311543d49ea54a1165acda0c4bb5c897576b
okravchenko <okravchenko@uma.tech>
2022-09-21 01:51:09 +0300

    first commit

---
+++ src/main/java/a2/tracker/TrackerApplication.java
@@ -0,0 +1,14 @@
+package a2.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackerApplication.class, args);
+	}
+
+
+}
